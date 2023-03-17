3425df67e6d5bdf4f0ab481452a111788433f755
Sahib Aliyev <sahib.aliyev.2002@gmail.com>
2023-03-17 14:06:48 +0400

    Done

---
+++ MarketStock.sql
@@ -0,0 +1,73 @@
+﻿CREATE DATABASE MarketStock
+
+USE MarketStock;
+
+CREATE TABLE Suppliers (
+  [SupplierID] INT PRIMARY KEY IDENTITY(1,1),
+  [SupplierName] NVARCHAR(MAX) NOT NULL CHECK ( [SupplierName] NOT LIKE N'')
+);
+
+CREATE TABLE ProductTypes (
+  [ProductTypeID] INT PRIMARY KEY IDENTITY(1,1),
+  [ProductTypeName] NVARCHAR(MAX) NOT NULL CHECK ( [ProductTypeName] NOT LIKE N'')
+);
+
+CREATE TABLE Products
+(
+    [ProductID]     INT PRIMARY KEY IDENTITY (1,1),
+    [ProductName]   NVARCHAR(MAX)  NOT NULL CHECK ( [ProductName] NOT LIKE N''),
+    [ProductTypeID] INT            NOT NULL FOREIGN KEY REFERENCES [ProductTypes] ([ProductTypeID]),
+    [SupplierID]    INT            NOT NULL FOREIGN KEY REFERENCES [Suppliers] ([SupplierID]),
+    [Cost]          DECIMAL(18, 2) NOT NULL
+);
+
+CREATE TABLE Deliveries (
+  [DeliveryID] INT PRIMARY KEY IDENTITY(1,1),
+  [ProductID] INT NOT NULL FOREIGN KEY REFERENCES [Products]([ProductID]),
+  [Quantity] INT NOT NULL,
+  [DeliveryDate] DATE NOT NULL
+);
+
+insert into Suppliers (SupplierName) values ('Kling LLC');
+insert into Suppliers (SupplierName) values ('Kuphal Inc');
+insert into Suppliers (SupplierName) values ('Jacobson');
+insert into Suppliers (SupplierName) values ('Reinge');
+insert into Suppliers (SupplierName) values ('Mante-Mohr');
+insert into Suppliers (SupplierName) values ('Crooks');
+insert into Suppliers (SupplierName) values ('Sahibo');
+
+insert into ProductTypes (ProductTypeName) values ('Chocolate');
+insert into ProductTypes (ProductTypeName) values ('Pecan Raisin');
+insert into ProductTypes (ProductTypeName) values ('Pepper');
+insert into ProductTypes (ProductTypeName) values ('Beef');
+insert into ProductTypes (ProductTypeName) values ('Cream');
+insert into ProductTypes (ProductTypeName) values ('Chocolate');
+insert into ProductTypes (ProductTypeName) values ('Pastry');
+insert into ProductTypes (ProductTypeName) values ('Pepper');
+insert into ProductTypes (ProductTypeName) values ('Apple');
+insert into ProductTypes (ProductTypeName) values ('Edible Flower');
+
+insert into Products (ProductName, ProductTypeID, SupplierID, Cost) values ('Milk, Callets', 1, 2, 31.0);
+insert into Products (ProductName, ProductTypeID, SupplierID, Cost) values ('Tarts', 2, 4, 16.92);
+insert into Products (ProductName, ProductTypeID, SupplierID, Cost) values ('Pablano', 3, 6, 1.55);
+insert into Products (ProductName, ProductTypeID, SupplierID, Cost) values ('Tongue, Fresh', 4, 3, 24.39);
+insert into Products (ProductName, ProductTypeID, SupplierID, Cost) values ('18%', 5, 1, 13.36);
+insert into Products (ProductName, ProductTypeID, SupplierID, Cost) values ('Pistoles, White', 6, 1, 39.39);
+insert into Products (ProductName, ProductTypeID, SupplierID, Cost) values ('Key Limepoppy Seed Tea', 7, 5, 30.88);
+insert into Products (ProductName, ProductTypeID, SupplierID, Cost) values ('Gypsy Pepper', 8, 3, 3.22);
+insert into Products (ProductName, ProductTypeID, SupplierID, Cost) values ('Royal Gala', 9, 2, 34.84);
+insert into Products (ProductName, ProductTypeID, SupplierID, Cost) values ('Mixed', 10, 6, 20.12);
+
+INSERT INTO Deliveries (ProductID, Quantity, DeliveryDate) VALUES (1, 10, '1-20-2023')
+INSERT INTO Deliveries (ProductID, Quantity, DeliveryDate) VALUES (2, 100, '1-1-2023')
+INSERT INTO Deliveries (ProductID, Quantity, DeliveryDate) VALUES (3, 15, '12-15-2022')
+INSERT INTO Deliveries (ProductID, Quantity, DeliveryDate) VALUES (4, 5, '2-20-2023')
+INSERT INTO Deliveries (ProductID, Quantity, DeliveryDate) VALUES (5, 13, '2-01-2023')
+INSERT INTO Deliveries (ProductID, Quantity, DeliveryDate) VALUES (6, 20, '2-20-2023')
+INSERT INTO Deliveries (ProductID, Quantity, DeliveryDate) VALUES (7, 30, '2-10-2023')
+INSERT INTO Deliveries (ProductID, Quantity, DeliveryDate) VALUES (8, 10, '2-16-2023')
+INSERT INTO Deliveries (ProductID, Quantity, DeliveryDate) VALUES (9, 60, '2-23-2023')
+INSERT INTO Deliveries (ProductID, Quantity, DeliveryDate) VALUES (10, 25, '2-24-2023')
+
+SELECT * FROM Deliveries JOIN Products ON Products.ProductTypeID = Deliveries.DeliveryID ORDER BY Quantity DESC
+
---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,259 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace MarketStock.View
+{
+    public partial class MainWindow : Window
+    {
+        public static List<string> SortByList = new() { "All info about products", "All product types", "All suppliers", "Quantity Max to Min", "Quantity Min to Max", "Price Max to Min", "Price Min to Max", "Oldest product", "Avg quantity by type of product"};
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SortCb.ItemsSource = SortByList;
+        }
+
+        private void Close(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void SortCb_Selected(object sender, SelectionChangedEventArgs e)
+        {
+            ConfigurationBuilder builder = new();
+            builder.AddJsonFile("appsettings.json");
+            var res = builder.Build();
+            SqlConnection conn = new(res.GetConnectionString("MarketStock"));
+
+            using (conn)
+            {
+                conn.Open();
+
+                int choice = SortCb.SelectedIndex;
+
+                switch (choice)
+                {
+                    case 0:
+                    {
+                        ShowAll(conn);
+                        break;
+                    }
+                    case 1:
+                    {
+                        ShowAllTypes(conn);
+                        break;
+                    }
+                    case 2:
+                    {
+                        ShowAllSuppliers(conn);
+                        break;
+                    }
+                    case 3:
+                    {
+                        ShowMaxMinQuantity(conn);
+                        break;
+                    }
+                    case 4:
+                    {
+                        ShowMinMaxQuantity(conn);
+                        break;
+                    }
+                    case 5:
+                    {
+                        ShowMaxMinPrice(conn);
+                        break;
+                    }
+                    case 6:
+                    {
+                        ShowMinMaxPrice(conn);
+                        break;
+                    }
+                    case 7:
+                    {
+                        ShowOldest(conn);
+                        break;
+                    }
+                    case 8:
+                    {
+                        ShowAvgByType(conn);
+                        break;
+                    }
+                }
+
+                if (conn.State == System.Data.ConnectionState.Open)
+                    MessageBox.Show("Success connection!");
+                else
+                    MessageBox.Show("Error connection!");
+
+            }
+        }
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            if (SortTextBox.Text != string.Empty)
+            {
+                ConfigurationBuilder builder = new();
+                builder.AddJsonFile("appsettings.json");
+                var res = builder.Build();
+                SqlConnection conn = new(res.GetConnectionString("MarketStock"));
+
+                using (conn)
+                {
+                    conn.Open();
+                    list.Items.Clear();
+                    using SqlCommand command = new($"SELECT * FROM Products " +
+                                                   $"JOIN ProductTypes ON Products.ProductTypeID = ProductTypes.ProductTypeID " +
+                                                   $"JOIN Suppliers ON Products.SupplierID = Suppliers.SupplierID " +
+                                                   $"WHERE ProductTypes.ProductTypeName LIKE N'{SortTextBox.Text}' OR Suppliers.SupplierName LIKE N'{SortTextBox.Text}'" , conn);
+
+                    SqlDataReader reader = command.ExecuteReader();
+
+
+                    list.Items.Add($"{reader.GetName(8)}  |  {reader.GetName(6)}  |  {reader.GetName(1)}  |  {reader.GetName(4)}");
+                    while (reader.Read())
+                    {
+                        list.Items.Add($"{reader[8]}  |  {reader[6]}  |  {reader[1]}  |  {reader[4]}");
+                    }
+                    if (conn.State == System.Data.ConnectionState.Open)
+                        MessageBox.Show("Success connection!");
+                    else
+                        MessageBox.Show("Error connection!");
+
+                }
+            }
+        }
+
+        public void ShowAll(SqlConnection conn)
+        {
+            list.Items.Clear();
+            using SqlCommand command = new("SELECT * FROM Products", conn);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            list.Items.Add($"{reader.GetName(0)}  |  {reader.GetName(1)}  |  {reader.GetName(2)}  |  {reader.GetName(3)}");
+            while (reader.Read())
+            {
+                list.Items.Add($"{reader[0]}  |  {reader[1]}  |  {reader[2]}  |  {reader[3]}");
+            }
+
+        }
+
+        public void ShowAllTypes(SqlConnection conn)
+        {
+            list.Items.Clear();
+            using SqlCommand command = new("SELECT [ProductTypeName] FROM ProductTypes", conn);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            list.Items.Add($"{reader.GetName(0)}");
+            while (reader.Read())
+            {
+                list.Items.Add($"{reader[0]}");
+            }
+
+        }
+
+        public void ShowAllSuppliers(SqlConnection conn)
+        {
+            list.Items.Clear();
+            using SqlCommand command = new("SELECT [SupplierName] FROM Suppliers", conn);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            list.Items.Add($"{reader.GetName(0)}");
+            while (reader.Read())
+            {
+                list.Items.Add($"{reader[0]}");
+            }
+        }
+
+        public void ShowMaxMinQuantity(SqlConnection conn)
+        {
+            list.Items.Clear();
+            using SqlCommand command = new("SELECT * FROM Deliveries JOIN Products ON Products.ProductTypeID = Deliveries.DeliveryID ORDER BY Quantity DESC", conn);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            list.Items.Add($"{reader.GetName(5)}  |  {reader.GetName(2)}  |  {reader.GetName(3)}  |  {reader.GetName(8)}");
+            while (reader.Read())
+            {
+                list.Items.Add($"{reader[5]}  |  {reader[2]}  |  {reader[3]}  |  {reader[8]}");
+            }
+        }
+
+        public void ShowMinMaxQuantity(SqlConnection conn)
+        {
+            list.Items.Clear();
+            using SqlCommand command = new("SELECT * FROM Deliveries JOIN Products ON Products.ProductTypeID = Deliveries.DeliveryID ORDER BY Quantity ASC", conn);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            list.Items.Add($"{reader.GetName(5)}  |  {reader.GetName(2)}  |  {reader.GetName(3)}  |  {reader.GetName(8)}");
+            while (reader.Read())
+            {
+                list.Items.Add($"{reader[5]}  |  {reader[2]}  |  {reader[3]}  |  {reader[8]}");
+            }
+        }
+
+        public void ShowMaxMinPrice(SqlConnection conn)
+        {
+            list.Items.Clear();
+            using SqlCommand command = new("SELECT * FROM Deliveries JOIN Products ON Products.ProductTypeID = Deliveries.DeliveryID ORDER BY Cost DESC", conn);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            list.Items.Add($"{reader.GetName(5)}  |  {reader.GetName(2)}  |  {reader.GetName(3)}  |  {reader.GetName(8)}");
+            while (reader.Read())
+            {
+                list.Items.Add($"{reader[5]}  |  {reader[2]}  |  {reader[3]}  |  {reader[8]}");
+            }
+        }
+
+        public void ShowMinMaxPrice(SqlConnection conn)
+        {
+            list.Items.Clear();
+            using SqlCommand command = new("SELECT * FROM Deliveries JOIN Products ON Products.ProductTypeID = Deliveries.DeliveryID ORDER BY Cost ASC", conn);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            list.Items.Add($"{reader.GetName(5)}  |  {reader.GetName(2)}  |  {reader.GetName(3)}  |  {reader.GetName(8)}");
+            while (reader.Read())
+            {
+                list.Items.Add($"{reader[5]}  |  {reader[2]}  |  {reader[3]}  |  {reader[8]}");
+            }
+        }
+
+        public void ShowOldest(SqlConnection conn)
+        {
+            list.Items.Clear();
+            using SqlCommand command = new("SELECT * FROM Deliveries JOIN Products ON Products.ProductTypeID = Deliveries.DeliveryID ORDER BY DeliveryDate ASC", conn);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            list.Items.Add($"{reader.GetName(5)}  |  {reader.GetName(2)}  |  {reader.GetName(3)}  |  {reader.GetName(8)}");
+            while (reader.Read())
+            {
+                list.Items.Add($"{reader[5]}  |  {reader[2]}  |  {reader[3]}  |  {reader[8]}");
+            }
+        }
+        public void ShowAvgByType(SqlConnection conn)
+        {
+            list.Items.Clear();
+            using SqlCommand command = new("SELECT ProductTypeName, AVG(Quantity) AS AvgQuantity FROM ProductTypes " +
+                                           "JOIN Products ON Products.ProductTypeID = ProductTypes.ProductTypeID " +
+                                           "JOIN Deliveries ON Deliveries.DeliveryID = ProductTypes.ProductTypeID " +
+                                           "GROUP BY ProductTypeName", conn);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            list.Items.Add($"{reader.GetName(0)}  |  {reader.GetName(1)}");
+            while (reader.Read())
+            {
+                list.Items.Add($"{reader[0]}  |  {reader[1]}");
+            }
+        }
+    }
+
+}
